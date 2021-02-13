a5598986705ff3e56b04b804a34cc974e125f073
yash10019coder <lit2020066@iiitl.ac.in>
2021-02-13 20:30:12 +0530

    login app

---
+++ app/src/main/java/com/yash10019coder/loginapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.yash10019coder.loginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
